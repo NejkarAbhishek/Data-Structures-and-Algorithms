0d91a6eadb6dce1d063dcb46922bae5f7c2cdfa8
NejkarAbhishek <abhishek.nejkar@gmail.com>
2025-02-20 11:23:10 -0500

    Time: 23 ms (54.40%) | Memory: 14.5 MB (5.23%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s)!=len(t):
+            return False
+        
+        countS,countT={},{}
+
+        for index in range(len(s)):
+            countS[s[index]]=1+countS.get(s[index],0)
+            countT[t[index]]=1+countT.get(t[index],0)
+        
+        for c in countS:
+            if countS[c]!=countT.get(c,0):
+                return False
+        
+        return True

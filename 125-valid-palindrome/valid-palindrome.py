10549f573e48f5b05f2ba5ebda2d1718a0c07b66
NejkarAbhishek <abhishek.nejkar@gmail.com>
2025-02-24 12:48:05 -0500

    Time: 41 ms (28.45%) | Memory: 12.8 MB (80.53%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        left,right=0,len(s)-1
+
+        while left < right:
+            while left < right and not self.alpha_Num(s[left]):
+                left+=1
+            
+            while right > left and not self.alpha_Num(s[right]):
+                right-=1
+
+            if s[left].lower()!=s[right].lower():
+                return False    
+            
+            left,right=left+1,right-1
+        
+        return True
+    
+    def alpha_Num(self,c):
+        return (ord('A') <= ord(c) <= ord('Z') or
+                ord('a') <= ord(c) <= ord('z') or
+                ord('0') <= ord(c) <= ord('9'))

c5290def1223bced7b3257ffc9ef46d2e3459e2e
Abhishek Nejkar <abhishek.nejkar@gmail.com>
2026-01-25 17:26:51 -0500

    Time: 11 ms (62.24%) | Memory: 19.9 MB (19.69%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -5,19 +5,19 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        count = {}
-        freq = [[] for i in range(len(nums)+1)]
+        map = {}
+        freq = [[] for i in range(len(nums) + 1)]
 
         for n in nums:
-            count[n] = 1 + count.get(n,0)
+            map[n] = 1 + map.get(n, 0)
 
-        for n,c in count.items():
+        for n,c in map.items():
             freq[c].append(n)
 
-        res=[]
-        for i in range(len(freq)-1, 0,-1):
+        res = []
+
+        for i in range(len(freq)-1,0,-1):
             for n in freq[i]:
                 res.append(n)
-                if len(res) ==k:
+                if len(res) == k:
                     return res
-                    

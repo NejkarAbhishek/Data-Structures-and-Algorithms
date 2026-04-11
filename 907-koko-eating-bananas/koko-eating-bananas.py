aa4cbaff3a78016685fd3594add5cebecd2c8800
Abhishek Nejkar <abhishek.nejkar@gmail.com>
2026-04-11 19:13:23 -0400

    Time: 171 ms (49.55%) | Memory: 20.7 MB (22.60%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,24 +1,20 @@
-import math
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l, r = 1, max(piles)
+        res = r
 
-class Solution(object):
-    def minEatingSpeed(self, piles, h):
-        """
-        :type piles: List[int]
-        :type h: int
-        :rtype: int
-        """
-        L,R=1,max(piles)
-        res=R
-       
-        while L<=R:
-            mid=(L+R)//2
-            hrs=0
-            for pile in piles:
-                hrs += math.ceil(pile / float(mid))
-            if hrs<=h:
-                res=mid
-                R=mid-1
+
+        while l<=r:
+            k=(l+r)//2
+            hours=0
+            for p in piles:
+                hours+= math.ceil(p/k)
+
+            if hours <=h:
+                res=min(res,k)
+                r=k-1
             else:
-                L=mid+1
-  
+                l=k+1
+            
+
         return res

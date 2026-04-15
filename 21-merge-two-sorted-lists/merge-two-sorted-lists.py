9564c6b279a4a3801862f8b092e26b621a8c0a1b
Abhishek Nejkar <abhishek.nejkar@gmail.com>
2026-04-15 13:03:47 -0400

    Time: 0 ms (100.00%) | Memory: 19.3 MB (78.32%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -1,32 +1,27 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def mergeTwoLists(self, list1, list2):
-        """
-        :type list1: Optional[ListNode]
-        :type list2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         temp = ListNode()
-        tail = temp
-        l1=list1
-        l2=list2
+        tail= temp
 
-        while l1 and l2:
-            if l1.val < l2.val:
-                tail.next = l1
-                l1 = l1.next
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next=list1
+                list1=list1.next
             else:
-                tail.next = l2
-                l2 = l2.next
-            tail = tail.next
+                tail.next=list2
+                list2=list2.next
+            tail=tail.next
         
-        if l1:
-            tail.next = l1
-        elif l2:
-            tail.next = l2
-    
+
+        if list1:
+            tail.next=list1
+        elif list2:
+            tail.next=list2
+
+
         return temp.next
